ed0bb75585a0fc50b4ea07477d6685b7844a961a
alejuu <contact@alejuu.com>
2020-01-31 19:44:37 -0600

    Install Plugin gatsby-source-filesystem
    Add gatsby-source-filesystem Plugin into Gatsby config - gatsby-config.js

--- gatsby-config.js
+++ gatsby-config.js
@@ -6,4 +6,13 @@
 
 module.exports = {
   /* Your site config here */
+  plugins: [
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `images`,
+        path: `${__dirname}/src/images/`,
+      }
+    }
+  ]
 }
