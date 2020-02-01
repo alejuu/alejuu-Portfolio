edce08571229b58a33d54526e09e1f99cf4b90a0
alejuu <contact@alejuu.com>
2020-01-31 23:06:55 -0600

    Add data to Work template
    This template will populate each individual entry

--- src/templates/work-template.js
+++ src/templates/work-template.js
@@ -1,8 +1,52 @@
 import React from 'react'
+import { graphql } from 'gatsby'
+import Layout from '../components/layout'
+import Image from 'gatsby-image'
+import { Link } from 'gatsby'
 
-const WorkTemplate = props => {
-  console.log(props)
-  return <div>Work template</div>
+const WorkTemplate = ({ data }) => {
+  const {
+    title,
+    image,
+    link,
+    description: { description }  
+  } = data.work
+
+  return (
+    <Layout>
+      <section>
+        <div>
+          <div>
+            <Image fluid={image.fluid} alt={title} />
+        
+            <h2>{title}</h2>
+          </div>
+          <p>{description}</p>
+          <span>{link}</span>
+          <Link to="/work">back to work</Link>
+        </div>
+      </section>
+    </Layout>
+  )
 }
+
+export const query = graphql`
+  query($slug:String!){
+    work:contentfulWork(slug:{eq:$slug}){
+      id:contentful_id
+      title
+      slug
+      image:featuredImage {
+        fluid {
+          ...GatsbyContentfulFluid_withWebp
+        }
+      }
+      link
+      description {
+        description
+      }   
+    }
+  }
+`
 
 export default WorkTemplate
