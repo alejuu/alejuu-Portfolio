9105a06ddbd31da1d9461e482f5a4f2db0144c64
alejuu <contact@alejuu.com>
2020-01-31 19:39:22 -0600

    Create pages and Layout
    Add Navbar component to layout component - layout.js

---
+++ src/pages/work.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react'
+import Layout from '../components/layout'
+
+export default class work extends Component {
+  render() {
+    return (
+      <Layout>
+        <h1>Work</h1>
+      </Layout>
+    )
+  }
+}
