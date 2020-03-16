51318ec096035c8e711ec0af8adcd5eb587133d9
alejuu <contact@alejuu.com>
2020-03-16 06:53:54 -0500

    Add delay to loading animation

--- src/components/Animations/Loading.js
+++ src/components/Animations/Loading.js
@@ -11,21 +11,19 @@ class Loading extends PureComponent {
     width: '100%',
     opacity: props.opacity,
     transition: 'opacity 0.5s',
-    animationDelay: '20s',
-    zIndex: 1
+    zIndex: props.zIndex
   })
 
   static styleCircle = ({ props }) => ({
-    border: '10px solid black',
+    display: 'flex',
+    justifyContent: 'center',
+    alignItems: 'center',
+    border: '10px solid white',
     borderRadius: '100%',
     height: props.diameter,
-    left: '50%',
     opacity: props.opacity,
-    position: 'absolute',
-    top: '50%',
-    transform: 'translateX(-50%) translateY(-50%)',
     width: props.diameter,
-    zIndex: 1
+    zIndex: props.zIndex
   })
 
   // handleRest = () => {
@@ -37,19 +35,32 @@ class Loading extends PureComponent {
       <div>
         <Spring
           reset
-          from={{ opacity: 1 }}
-          to={{ opacity: 0 }}
+          from={{ opacity: 1, zIndex: 1 }}
+          to={{ opacity: 0, zIndex: -1 }}
+          config={{ tension: 1, friction: 1, precision: 0.1, delay: 50 }}
         >
           {props => <div style={Loading.styleBackground({ props })} />}
         </Spring>
-        <Spring
-          reset
-          from={{ opacity: 1, diameter: 0 }}
-          to={{ opacity: 0, diameter: 100 }}
-          // onRest={this.handleRest}
-        >
-          {props => <div style={Loading.styleCircle({ props })} />}
-        </Spring>
+        <div className='absolute top-0 left-0 w-full h-full -z-10'>
+          <div className='grid grid-cols-6 row-gap-0 gap-4 h-full'>
+            <div className='h-screen-1/2 md:h-full col-span-6 sm:col-span-6 md:col-span-2 lg:col-span-2 xl:col-span-2'>
+              <div className='flex flex-col items-center justify-center h-full'>
+                <Spring
+                  reset
+                  from={{ opacity: 1, diameter: 100, zIndex: 1 }}
+                  to={{ opacity: 0, diameter: 500, zIndex: -1 }}
+                  // onRest={this.handleRest}
+                >
+                  {props => 
+                    <div style={Loading.styleCircle({ props })}>
+                      <span className='font-serif text-6xl text-white'>A</span>
+                    </div>
+                  }
+                </Spring>
+              </div>
+            </div>
+          </div>
+        </div>
       </div>    
     )
   }
--- src/pages/index.js
+++ src/pages/index.js
@@ -12,32 +12,32 @@ const index = () => {
         title='Home'
       />
       <section className='w-full h-full'>
-      <div className='grid grid-cols-6 row-gap-0 gap-4 h-full'>
-        <div className='h-screen-1/2 md:h-full col-span-6 sm:col-span-6 md:col-span-2 lg:col-span-2 xl:col-span-2 bg-black'>
-          <div className='flex flex-col items-center h-full'>
-            <div className='invisible h-20 w-20 mt-auto'/>
-            <div className='rounded-full h-32 w-32 md:h-48 md:w-48 flex items-center justify-center bg-white'>
-              <span className='font-serif font-bold text-6xl tracking-tight underline'>A</span>
+        <div className='grid grid-cols-6 row-gap-0 gap-4 h-full'>
+          <div className='h-screen-1/2 md:h-full col-span-6 sm:col-span-6 md:col-span-2 lg:col-span-2 xl:col-span-2 bg-black'>
+            <div className='flex flex-col items-center h-full'>
+              <div className='invisible h-20 w-20 mt-auto'/>
+              <div className='rounded-full h-32 w-32 md:h-48 md:w-48 flex items-center justify-center bg-white'>
+                <span className='font-serif font-bold text-6xl tracking-tight underline'>A</span>
+              </div>
+              <div className='text-center md:text-left h-20 mb-auto'>
+                <h1 className='font-serif font-bold text-2xl md:text-3xl text-white mt-2 md:mt-4'>Alejandro Juarez</h1>
+                <h2 className='font-normal text-1xl md:text-2xl text-white'>Web Developer</h2>
+              </div>
             </div>
-            <div className='text-center md:text-left h-20 mb-auto'>
-              <h1 className='font-serif font-bold text-2xl md:text-3xl text-white mt-2 md:mt-4'>Alejandro Juarez</h1>
-              <h2 className='font-normal text-1xl md:text-2xl text-white'>Web Developer</h2>
+          </div>
+          <div className='h-screen-1/2 md:h-full col-span-6 sm:col-span-6 md:col-span-4 lg:col-span-4 xl:col-span-4'>
+            <div className='flex flex-col items-center justify-center h-full'>
+              <div className='max-w-screen-sm w-1/2 text-center md:text-left bg-black text-white p-4'>
+                <h3 className='text-4xl md:text-6xl'>About</h3>
+                <h3 className='text-4xl md:text-6xl'>
+                  <SpringLink to='/work'>Work</SpringLink>
+                </h3>
+                <h3 className='text-4xl md:text-6xl'>Contact</h3>
+              </div>
+              <Loading />
             </div>
           </div>
         </div>
-        <div className='h-screen-1/2 md:h-full col-span-6 sm:col-span-6 md:col-span-4 lg:col-span-4 xl:col-span-4'>
-          <div className='flex flex-col items-center justify-center h-full'>
-            <div className='max-w-screen-sm w-1/2 text-center md:text-left bg-black text-white p-4'>
-              <h3 className='text-4xl md:text-6xl'>About</h3>
-              <h3 className='text-4xl md:text-6xl'>
-                <SpringLink to='/work'>Work</SpringLink>
-              </h3>
-              <h3 className='text-4xl md:text-6xl'>Contact</h3>
-            </div>
-            <Loading />
-          </div>
-        </div>
-      </div>
       </section>   
     </Layout>
   )
