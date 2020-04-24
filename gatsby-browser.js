85c9d073aa14d9824c52e6cc941690c6f5173843
alejuu <contact@alejuu.com>
2020-04-24 15:41:42 -0500

    Add Global State to make it persist between page change
    Following this tutorial https://dev.to/changoman/gatsby-js-global-state-w-react-context-usereducer-3c1
    Add toggle to switch color theme option (color not changing, just an example)

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -1,1 +1,8 @@
-import "./src/css/style.css";
+import './src/css/style.css'
+
+import React from 'react'
+import GlobalContextProvider from './src/context/GlobalContextProvider'
+
+export const wrapRootElement = ({element }) => {
+  return <GlobalContextProvider>{element}</GlobalContextProvider>
+}
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,9 +1,16 @@
 import { graphql, useStaticQuery } from 'gatsby'
-import React, { useState } from 'react'
+import React, { useState, useContext } from 'react'
 import { globalHistory as history } from '@reach/router'
 import { SpringLink } from '../components/react-spring-animation'
 
+import { GlobalDispatchContext, GlobalStateContext } from '../context/GlobalContextProvider'
+
 const Navbar = () => {
+  const dispatch = useContext(GlobalDispatchContext)
+  const state = useContext(GlobalStateContext)
+  // console.log(state)
+  // console.log(dispatch)
+
   const [isExpanded, toggleExpansion] = useState(false)
   const { site } = useStaticQuery(graphql`
     query SiteTitleQuery {
@@ -37,38 +44,46 @@ const Navbar = () => {
           </span>
         </SpringLink>
       </div>
-      <div className={`${navCurrent} flex-wrap items-center justify-end fixed z-50 w-full top-0 mx-auto p-2`}>
-        <button
-          className='block md:hidden border border-white flex items-center px-3 py-2 rounded'
-          onClick={() => toggleExpansion(!isExpanded)}
-        >
-          <svg
-            className='fill-current h-3 w-3'
-            viewBox='0 0 20 20'
-            xmlns='http://www.w3.org/2000/svg'
+      <div className='fixed z-50 w-full top-0 mx-auto'>
+        <div className={`${navCurrent} flex-wrap items-center justify-end p-2`}>
+          <button
+            className='block md:hidden border border-white flex items-center px-3 py-2 rounded'
+            onClick={() => toggleExpansion(!isExpanded)}
           >
-            <title>Menu</title>
-            <path d='M0 3h20v2H0V3zm0 6h20v2H0V9zm0 6h20v2H0v-2z' />
-          </svg>
+            <svg
+              className='fill-current h-3 w-3'
+              viewBox='0 0 20 20'
+              xmlns='http://www.w3.org/2000/svg'
+            >
+              <title>Menu</title>
+              <path d='M0 3h20v2H0V3zm0 6h20v2H0V9zm0 6h20v2H0v-2z' />
+            </svg>
+          </button>
+          <nav
+            className={`${
+              isExpanded ? `block` : `hidden`
+            } md:block md:flex md:items-center w-full md:w-auto`}
+          >
+            {[
+              {
+                route: `/`,
+                title: `Home`
+              },
+              {
+                route: `/work`,
+                title: `Work`
+              },
+            ].map(link => (
+              <SpringLink className='block md:inline-block mt-4 md:mt-0 md:ml-6 font-serif text-black' activeClassName='underline' key={link.title} to={link.route}>{link.title}</SpringLink>
+            ))}
+          </nav>
+        </div>
+        {state.theme}
+        <button type='button' onClick={() => {
+          dispatch({type: 'TOGGLE_THEME'})
+        }}>
+          Toggle Theme
         </button>
-        <nav
-          className={`${
-            isExpanded ? `block` : `hidden`
-          } md:block md:flex md:items-center w-full md:w-auto`}
-        >
-          {[
-            {
-              route: `/`,
-              title: `Home`
-            },
-            {
-              route: `/work`,
-              title: `Work`
-            },
-          ].map(link => (
-            <SpringLink className='block md:inline-block mt-4 md:mt-0 md:ml-6 font-serif text-black' activeClassName='underline' key={link.title} to={link.route}>{link.title}</SpringLink>
-          ))}
-        </nav>
       </div>
     </header>
   )
